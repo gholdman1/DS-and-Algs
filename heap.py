7aa735903baaf06bd5a5c5234cebcad547e3b5d5
G Holdman <gholdman@protonmail.com>
2020-02-20 17:10:43 -0600

    Add heap notes

---
+++ heap.py
@@ -0,0 +1,23 @@
+class Heap:
+    '''
+    Also known as a priority queue.
+    '''
+
+    def __init__(self):
+
+        pass
+
+    def insert(self,x):
+        pass
+
+    def heapify(self,arr):
+        pass
+
+    def pop(self):
+        '''
+        Return largest value
+        '''
+        pass
+
+    def size(self):
+        pass
