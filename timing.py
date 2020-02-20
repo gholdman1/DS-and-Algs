871db093e0a84746b570d76333b717931de360d2
G Holdman <gholdman@protonmail.com>
2020-02-20 07:38:27 -0600

    Add an object that can time algorithms on data structures

---
+++ timing.py
@@ -0,0 +1,16 @@
+
+
+class Timer:
+
+    def __init__(self,ds):
+        '''
+        Initialized with a data structure to test
+
+        ds assumed to have the following methods:
+        - insert
+        - delete
+        - search
+        '''
+    
+    def insert(self):
+        
