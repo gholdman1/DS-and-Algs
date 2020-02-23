c09ecf7b45c07b763b8f4b9d33f4fa9447bcd312
G Holdman <gholdman@protonmail.com>
2020-02-23 08:44:00 -0600

    Reorganize sorting

--- dsalgs/sorting.py
+++ dsalgs/sorting.py
@@ -1,5 +1,63 @@
-# Sorting algorithms
+###############################
+# SORTING ALGORITHMS
+# Algorithms sorted by time complexity
+# (dictionary sorting of (WORST, AVE, BEST))
+############################
 
+## Useful functions
+def swap(arr,i,j):
+    temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+
+    return arr
+
+## O(n^2)
+
+# Selection Sort
+
+def selectionsort(arr):
+    '''
+    Partition array into an unsorted and sorted portion.
+    Find smallest value in unsorted portion and place at end of sorted.
+
+    Time complexity:    
+    '''
+    n=len(arr)
+    for i in range(n):
+        # Find index of minimum
+        minidx=i
+        for j in range(i,n):
+            if arr[j]<arr[minidx]:
+                minidx=j
+
+        swap(arr,minidx,i)
+
+    return arr
+    
+# Bubble Sort
+
+def bubblesort(arr):
+    '''
+    Time complexity:    O(n^2) Backwards, so every key is compared to every other key.
+                        O(n^2) On average, ???
+    Space complexity:   O(1)
+                        ???
+    '''
+    n=len(arr)
+    for i in range(n):
+
+        for j in range(0,n-i-1):
+
+            if arr[j]>arr[j+1]:
+                arr=swap(arr,j,j+1)
+
+    return arr
+
+# Insertion Sort
+
+
+## O(n log(n))
 
 
 # Merge sort
@@ -42,12 +100,6 @@ def mergesort(arr):
     return arr
 
 # Quicksort
-def swap(arr,i,j):
-    temp=arr[i]
-    arr[i]=arr[j]
-    arr[j]=temp
-
-    return arr
 
 def partition(arr):
     
@@ -76,17 +128,6 @@ def quicksort(arr):
         else:
             arr2=quicksort(arr[i+1:])
         arr=arr1+[arr[i]]+arr2
-
-    return arr
-
-def bubblesort(arr):
-    n=len(arr)
-    for i in range(n):
-
-        for j in range(0,n-i-1):
-
-            if arr[j]>arr[j+1]:
-                arr=swap(arr,j,j+1)
 
     return arr
 
