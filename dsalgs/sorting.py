971385fa263bbf65ff3ff80251b77845484e72cd
G Holdman <gholdman@protonmail.com>
2020-02-26 16:08:43 -0600

    Make many changes to sorting

--- dsalgs/sorting.py
+++ dsalgs/sorting.py
@@ -4,49 +4,10 @@
 # (dictionary sorting of (WORST, AVE, BEST))
 ############################
 
-## Useful functions
-def swap(arr,i,j):
-    temp=arr[i]
-    arr[i]=arr[j]
-    arr[j]=temp
+## Imports
 
-    return arr
-
-def merge(L,R):
-
-    i=0
-    j=0
-
-    arr=[]
-    while i < len(L) and j < len(R):
-        if L[i]<=R[j]:
-            arr.append(L[i])
-            i+=1
-        else:
-            arr.append(R[j])
-            j+=1
-   
-    while i < len(L):
-        arr.append(L[i])
-        i+=1
-    while j < len(R):
-        arr.append(R[j])
-        j+=1
-
-    return arr
-
-def partition(arr):
-    
-    pivot=arr[-1]
-
-    i=-1
-    for j in range(len(arr)-1):
-        if arr[j]<=pivot:
-            i+=1
-            arr=swap(arr,i,j)
-    arr=swap(arr,i+1,len(arr)-1)
-    
-    return arr, i+1
+from dsalgs.utilities import swap, merge, partition
+from dsalgs.heap import Heap
 
 # Selection Sort
 
@@ -128,6 +89,32 @@ def bubblesort(arr):
     return arr
 
 
+# Quicksort
+
+def quicksort(arr):
+    '''
+
+
+    Time complexity:    O(n^2), if 
+                        O(n log(n)) on average
+                        O(n log(n)) at best
+    '''
+
+    if len(arr)>1:
+        arr, i = partition(arr)
+
+        if i == 0:
+            arr1=[]
+        else:
+            arr1=quicksort(arr[:i])
+        if i == len(arr)-1:
+            arr2=[]
+        else:
+            arr2=quicksort(arr[i+1:])
+        arr=arr1+[arr[i]]+arr2
+
+    return arr
+
 # Merge sort
 
 def mergesort(arr):
@@ -155,25 +142,18 @@ def mergesort(arr):
 
     return arr
 
-# Quicksort
 
-def quicksort(arr):
+def heapsort(arr):
     '''
-
+    An improved version of selectionsort. Utilizes a heap to find largest element
     '''
+    n=len(arr)
+    h=Heap(n)
 
-    if len(arr)>1:
-        arr, i = partition(arr)
+    h.make_heap(arr)
 
-        if i == 0:
-            arr1=[]
-        else:
-            arr1=quicksort(arr[:i])
-        if i == len(arr)-1:
-            arr2=[]
-        else:
-            arr2=quicksort(arr[i+1:])
-        arr=arr1+[arr[i]]+arr2
+    for i in range(n):
+        arr[i]=h.extract_min()
 
     return arr
 
@@ -182,10 +162,4 @@ def timsort(arr):
     Based upon insertionsort and mergesort
     '''
 
-    pass
-
-def heapsort(arr):
-    '''
-    An improved version of selectionsort. Utilizes a heap to find largest element
-    '''
     pass
