6bed4012bc25fd2aa19bedc9712655e2a638d9f6
G Holdman <gholdman@protonmail.com>
2020-02-27 15:08:08 -0600

    Create TestLinkedList class

--- tests/test_linkedlist.py
+++ tests/test_linkedlist.py
@@ -1,10 +1,19 @@
-"""Tests the linkedlist file"""
+"""Tests linkedlist.py"""
 
 import pytest
 import sys,os
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+# Add dsalgs package to path
+filepath=os.path.abspath(__file__)
+dsalgs_path=os.path.dirname(os.path.dirname(filepath))
+sys.path.append(dsalgs_path)
 
-from dsalgs import linkedlist
+from dsalgs.linkedlist import LinkedList
 
+class TestLinkedList:
 
+	def __init__(self):
+		self.test_ds=LinkedList()
+
+	def test_insert(self):
+		pass
